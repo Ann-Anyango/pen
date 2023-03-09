e6d7864d5cb7c05e8f8450a9d43ce228f93a093c
Ann-Anyango <annmwangombe@gmail.com>
2023-03-09 09:10:45 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,71 @@
+fun main() {
+    val word ="what?"
+    println(word)
+    val name="Ann"
+    //immutable variables
+    val number:String= "agentNumber"
+    println(number)
+    println(name)
+    println(age)
+    println(student)
+    println(worldPopuplation)
+    var  num1=12
+    var num2=14
+    var addition =num1+num2
+    println(addition)
+    var num3=45
+    var num4=5
+    var modole=num3%num4
+    println(modole)
+    var a=5
+    var b=3
+    var f=15
+    var e=34
+    var module=f-e
+    println(modole)
+    multiply(34,45)
+    println("Ann")
+
+println(text[0])
+    println(text[1])
+    println(text[2])
+    println(text[3])
+    println(text[4])
+    println(text[5])
+    println(text[6])
+    println(sentence)
+    println(x.uppercase())
+    println(x.lowercase())
+    println(x.uppercase())
+    println(tent[1])
+    println(tent[3])
+    println(tent[4])
+    println(nam.max())
+    println(nam.average())
+    println(nam.min())
+
+
+
+
+}
+var age: Byte= 127
+var student:Short= 32
+var worldPopuplation:Float= 20.45F
+  fun multiply( num1:Int,num2:Int){
+      var product=num1*num2
+      println()
+  }
+fun greetingPerson( name:String){
+    println("Hello"+ name)
+
+}
+var text="Electricity"
+var name="Winny"
+var Age=20
+var sentence="I am $name i am $Age"
+var x="women technology"
+var tent="AkiraChix"
+var nam= arrayOf(1,2,3,4,5,40)
+
+
+
